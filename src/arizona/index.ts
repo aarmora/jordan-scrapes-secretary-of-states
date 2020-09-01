947f1833e68b61f7dc2501c73a50d22a589071a9
Jacob Terrell <jacobit97@hotmail.com>
2020-08-31 19:19:43 -0600

    Formatting fixes

--- src/arizona/index.ts
+++ src/arizona/index.ts
@@ -3,18 +3,18 @@ import axios from "axios";
 import { timeout } from "../helpers";
 
 (async () => {
-	const startingId = 2177292;
-	for (let i = 0; i <= 20; i++ ) {
+    const startingId = 2177292;
+    for (let i = 0; i <= 20; i++) {
         await getDetails(startingId + i);
         //Longer timeout needed because of DDOS protection from website
-		await timeout(5000);
-	}
+        await timeout(5000);
+    }
 })();
 
 async function getDetails(sosId: number) {
-	const axiosResponse = await axios.get(`https://ecorp.azcc.gov/BusinessSearch/BusinessInformation?businessId=${sosId}`);
-	const $ = cheerio.load(axiosResponse.data);
-	const title = $(".data_pannel1 .row:nth-of-type(4) .col-xs-12.col-sm-4").text().replace(/\n/g, "").trim();
+    const axiosResponse = await axios.get(`https://ecorp.azcc.gov/BusinessSearch/BusinessInformation?businessId=${sosId}`);
+    const $ = cheerio.load(axiosResponse.data);
+    const title = $(".data_pannel1 .row:nth-of-type(4) .col-xs-12.col-sm-4").text().replace(/\n/g, "").trim();
     const formationDate = $(".data_pannel1 .row:nth-of-type(6) .col-xs-12:nth-of-type(2)").text().replace(/\n/g, "").trim();
     const status = $(".data_pannel1 .row:nth-of-type(5) .col-xs-12:nth-of-type(4)").text().replace(/\n/g, "").trim();
     const businessType = $(".data_pannel1 .row:nth-of-type(9) .col-xs-12:nth-of-type(2)").text().replace(/\n/g, "").trim();
@@ -24,8 +24,8 @@ async function getDetails(sosId: number) {
     const email = $(".data_pannel1 .row:nth-of-type(17) .col-xs-12:nth-of-type(4)").text().replace(/\n/g, "").trim();
 
     const business: any = {};
-	business.title = title;
-	business.formationDate = formationDate;
+    business.title = title;
+    business.formationDate = formationDate;
     business.sosId = sosId;
     business.status = status;
     business.businessType = businessType;
@@ -33,5 +33,5 @@ async function getDetails(sosId: number) {
     business.address = address;
     business.email = email
 
-	console.log("business", business);
+    console.log("business", business);
 }
